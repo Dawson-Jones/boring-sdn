7d17a786f87a3c2809a697808f5736017f20cdab
jiadongqing <jiadongqing@bytedance.com>
2023-07-02 03:42:37 +0800

    feat: tun multi queue 1. complete command parse, now can use config file or command 2. tun use multi queue

--- boring-sdn/src/main.rs
+++ boring-sdn/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::SocketAddr;
-use std::net::{Ipv4Addr, IpAddr};
+use std::sync::Arc;
 
+use futures::future;
 use socket2::{Socket, Domain, Type, SockAddr};
 use tokio::net::UdpSocket;
 use tokio_tun::{TunBuilder, Tun};
@@ -22,74 +23,79 @@ const MAX_PACKET_LEN: usize = 1500;
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    let matches = parse::parse_command();
+    let command = parse::parse_command();
 
-    // TODO: 迁移到 parse 里面, 使用结构体输出结构
-    let tun_name = matches.get_one::<String>("tun_name").unwrap();
-    let tun_ip = matches.get_one::<String>("tun_ip").unwrap().parse::<Ipv4Addr>().unwrap();
-    // let tpe = matches.get_one::<String>("type").unwrap();
-    let listen = matches.get_one::<String>("listen").unwrap().parse::<u16>().unwrap();
-
-    let tun: Tun = TunBuilder::new()
-        .name(&tun_name)
-        // TODO: 可以在内部或设置
-        .address(tun_ip)
-        // TODO: 命令行 tun_ip 设置 192.168.1.5/24
-        .netmask("255.255.255.0".parse::<Ipv4Addr>().unwrap())
-        // .mtu(mtu)
-        .tap(false)
+    let num_cpus = num_cpus::get();
+    log::info!("cpus: {}", num_cpus);
+    let mut tun_builder = TunBuilder::new()
+        .name(&command.tun_name)
+        .mtu(command.mtu)
+        .tap(if command.tun_mode == "tap" {true} else {false})
         .packet_info(false)
-        .up()
-        .try_build()
-        .unwrap();
+        .up();
+    if let Some((ip, mask)) = command.tun_ip_cidr {
+        tun_builder = tun_builder.address(ip).netmask(mask);
+    }
+    let tuns = tun_builder.try_build_mq(num_cpus).unwrap();
     log::info!("tun interface built");
 
-    let local_addr = SocketAddr::from(("0.0.0.0".parse::<IpAddr>().unwrap(), listen));
-    let udp_srv = new_udp_reuseport(local_addr);
-    log::info!("udp server: {} created", local_addr.to_string());
+    let tuns: Vec<Arc<Tun>> = tuns.into_iter().map(Arc::new).collect();
+    let mut handles = Vec::new();
+    for i in 0..num_cpus {
+        let tun = tuns[i].clone();
 
-    let mut buf_from_local = [0u8; MAX_PACKET_LEN];
-    let mut buf_from_remote = [0u8; MAX_PACKET_LEN];
-    // TODO: 
-    //  try multiple, 
-    loop {
-        tokio::select! {
-            // if let Ok(size) = tun.recv(&mut buf_from_local).await {
-            Ok(size) = tun.recv(&mut buf_from_local) => {
-                let (buf, remote_addr) = simple_route::route_from_local(&mut buf_from_local[..size]);
+        let h = tokio::spawn(async move {
+            let mut buf_from_local = [0u8; MAX_PACKET_LEN];
+            let mut buf_from_remote = [0u8; MAX_PACKET_LEN];
+            let udp_srv = new_udp_reuseport(SocketAddr::V4(command.listen));
+            log::info!("cpu {}, udp server: {} created", i, command.listen.to_string());
 
-                // TODO: restore the UDP socket in somewhere 
-                //  to avoid calling syscall when creating udp_cli
-                let udp_cli = new_udp_cli(remote_addr);
-                match udp_cli.send(buf).await {
-                    Ok(size) => {
-                        log::info!("send packet {} to {}", size, remote_addr);
-                    },
-                    Err(e) => {
-                        log::error!("send packet error: {}", e);
+            loop {
+                tokio::select! {
+                    // if let Ok(size) = tun.recv(&mut buf_from_local).await {
+                    Ok(size) = tun.recv(&mut buf_from_local) => {
+                        let (buf, remote_addr) = simple_route::route_from_local(&mut buf_from_local[..size]);
+
+                        // TODO: restore the UDP socket in somewhere 
+                        //  to avoid calling syscall when creating udp_cli
+                        let udp_cli = new_udp_cli(remote_addr);
+                        match udp_cli.send(buf).await {
+                            Ok(size) => {
+                                log::info!("send packet {} to {}", size, remote_addr);
+                            },
+                            Err(e) => {
+                                log::error!("send packet error: {}", e);
+                            }
+                        }
+                    }
+
+                    // if let Ok((size, remote_addr)) = udp_srv.recv_from(&mut buf_from_remote).await {
+                    Ok((size, _remote_addr)) = udp_srv.recv_from(&mut buf_from_remote) => {
+                        let (buf, forward) = simple_route::route_from_remote(&mut buf_from_remote[..size]);
+                        if let Some(addr) = forward {
+                            // TODO: see log
+                            log::warn!("relay node, forward to {}", addr)
+                        } else {
+                            match tun.send(buf).await {
+                                Ok(size) => {
+                                    log::info!("packet {} up to application", size);
+                                },
+                                Err(e) => {
+                                    log::error!("packet up to error: {}", e);
+                                },
+                            }
+                        }
                     }
                 }
             }
 
-            // if let Ok((size, remote_addr)) = udp_srv.recv_from(&mut buf_from_remote).await {
-            Ok((size, _remote_addr)) = udp_srv.recv_from(&mut buf_from_remote) => {
-                let (buf, forward) = simple_route::route_from_remote(&mut buf_from_remote[..size]);
-                if let Some(addr) = forward {
-                    // TODO: see log
-                    log::warn!("relay node, forward to {}", addr)
-                } else {
-                    match tun.send(buf).await {
-                        Ok(size) => {
-                            log::info!("packet {} up to application", size);
-                        },
-                        Err(e) => {
-                            log::error!("packet up to error: {}", e);
-                        },
-                    }
-                }
-            }
-        }
+        });
+
+        handles.push(h);
     }
+
+    let futures = future::join_all(handles).await;
+    println!("{:?}", futures);
 }
 
 
--- boring-sdn/src/parse.rs
+++ boring-sdn/src/parse.rs
@@ -1,49 +1,197 @@
-pub fn parse_command() -> clap::ArgMatches {
+use std::{net::{Ipv4Addr, SocketAddrV4}, fs};
+
+use serde::{Serialize, Deserialize};
+
+
+fn tun_mode_default() -> String {
+    String::from("tun")
+}
+
+fn tun_name_default() -> String {
+    String::from("")
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Config {
+    #[serde(default = "tun_name_default")]
+    tun_name: String,
+    tun_ip_cidr: Option<String>,
+    #[serde(default = "tun_mode_default")]
+    tun_mode: String,
+    listen: String,
+    mtu: Option<i32>,
+}
+
+
+pub struct Command {
+    pub tun_name: String,
+    pub tun_ip_cidr: Option<(Ipv4Addr, Ipv4Addr)>,
+    pub tun_mode: String,
+    pub listen: SocketAddrV4,
+    pub mtu: i32,
+}
+
+fn cidr_to_netmask(cidr: u8) -> Ipv4Addr {
+    let mask: u32 = 0xFFFFFFFF << (32 - cidr);
+    Ipv4Addr::from(mask)
+}
+
+
+fn split_ip_cidr(s: &str) -> (Ipv4Addr, Ipv4Addr) {
+    let ip_cidr: Vec<&str> = s.split("/").collect();
+    
+    let ip = ip_cidr[0].parse::<Ipv4Addr>().unwrap();
+    if ip_cidr.len() > 1 {
+        let cidr = ip_cidr[1].parse::<u8>().unwrap();
+        (ip, cidr_to_netmask(cidr))
+    } else {
+        (ip, cidr_to_netmask(32))
+    }
+}
+
+fn split_ip_port(s: &str) -> SocketAddrV4 {
+    let ip_cidr: Vec<&str> = s.split(":").collect();
+    let ip;
+    let port;
+    
+    if ip_cidr.len() > 1 {
+        ip = ip_cidr[0].parse::<Ipv4Addr>().unwrap();
+        port = ip_cidr[1].parse::<u16>().unwrap();
+    } else {
+        ip = "0.0.0.0".parse::<Ipv4Addr>().unwrap();
+        port = ip_cidr[0].parse::<u16>().unwrap();
+    }
+
+    SocketAddrV4::new(ip, port)
+}
+
+fn usage() -> clap::ArgMatches {
     let matches = clap::Command::new("Boring-SDN")
         .version(clap::crate_version!())
         .author("Dawson Jones (jiadongqing@bytedance.com)")
-        // TODO:
-        // .arg(
-        //     clap::Arg::new("config")
-        //         .long("config")
-        //         .short('c')
-        //         .required(false)
-        //         .value_name("PATH")
-        //         .help("read configure from File")
-        // )
+        .arg(
+            clap::Arg::new("config")
+                .long("config")
+                .short('c')
+                .required(false)
+                .value_name("PATH")
+                .help("read configure from File")
+        )
         .arg(
             clap::Arg::new("tun_name")
                 .long("tun")
                 .required(false)
                 .value_name("tunX")
                 .help("Sets the Tun interface name, if absent, pick the next available name")
-                .default_value("")
         )
         .arg(
             clap::Arg::new("tun_ip")
                 .long("tun-ip")
                 .required(false)
-                .value_name("IP")
-                .help("Sets the Tun interface address")
+                .value_name("IP/CIDR")
+                .help("Sets the Tun interface address in CIDR format, if CIDR is absent, it defaults to use 32.")
         )
         .arg(
-            clap::Arg::new("type")
+            clap::Arg::new("mode")
                 .long("type")
                 .required(false)
                 .value_name("tun/tap")
                 .help("Set type of interface. default: tun")
-                .default_value("tun")
         )
         .arg(
             clap::Arg::new("listen")
                 .long("listen")
                 .short('l')
-                .required(true)
+                .required(false)
                 .value_name("PORT")
                 .help("which port be listened, default: 55001")
-                .default_value("55001")
+                // .default_value("55001")
+        )
+        .arg(
+            clap::Arg::new("mtu")
+                .long("mtu")
+                .value_name("mtu")
+                .required(false)
+                .help("default: 1400")
         )
         .get_matches();
 
     matches
+}
+
+pub fn parse_command() -> Command {
+    let matches = usage();
+
+    let mut command = Command {
+        tun_name: "tun0".to_string(),
+        tun_ip_cidr: None,
+        tun_mode: "tun".to_string(),
+        listen: SocketAddrV4::new("0.0.0.0".parse::<Ipv4Addr>().unwrap(), 55001),
+        mtu: 1400,
+    };
+
+    let config_str= matches.get_one::<String>("config").map(fs::read).transpose().unwrap();
+    if let Some(p) = config_str {
+        log::info!("--config specified");
+
+        let config: Config = toml::from_slice(p.as_slice()).unwrap();
+        log::info!("config: {:#?}", config);
+
+        command.tun_name = config.tun_name;
+        if let Some(ip_cidr) = config.tun_ip_cidr {
+            command.tun_ip_cidr = Some(split_ip_cidr(&ip_cidr));
+        }
+        if let Some(mtu) = config.mtu{
+            command.mtu = mtu;
+        }
+
+        command.tun_mode = config.tun_mode;
+
+        command.listen = split_ip_port(&config.listen);
+    }
+
+    if let Some(tun_name) = matches.get_one::<String>("tun_name") {
+        command.tun_name = tun_name.to_string();
+        log::info!("--command-tun_name specified: {}", tun_name);
+    }
+    if let Some(ip_cidr) = matches.get_one::<String>("tun_ip") {
+        command.tun_ip_cidr = Some(split_ip_cidr(&ip_cidr));
+        log::info!("--command-tun_ip specified: {}", ip_cidr);
+    }
+    if let Some(mode) = matches.get_one::<String>("mode") {
+        log::info!("--command-mode specified: {}", mode);
+        if mode == "tap" {
+            command.tun_mode = mode.to_string();
+        }
+    }
+    if let Some(listen) = matches.get_one::<String>("listen") {
+        command.listen = split_ip_port(listen);
+        log::info!("--command-listen specified: {}", command.listen.to_string());
+    }
+    if let Some(mtu) = matches.get_one::<String>("mtu") {
+        command.mtu = mtu.parse::<i32>().unwrap();
+        log::info!("--command-mtu specified: {}", mtu);
+    }
+
+    command
+}
+
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_config() {
+        // read current package
+        let content = fs::read("../config.toml").unwrap();
+        let config: Config = toml::from_slice(content.as_slice()).unwrap();
+        println!("{:#?}", config);
+    }
+
+    #[test]
+    fn test_cidr() {
+        let cidr = 12;
+        let ip = cidr_to_netmask(cidr);
+        println!("cidr: {cidr}, mask: {ip}");
+    }
 }
